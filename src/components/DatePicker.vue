3202202991a2909aeae53d21e3fed9ad6425417c
MehranTaheri <mehrant7510@gmail.com>
2021-06-14 15:46:35 +0430

    Fix margin

--- src/components/DatePicker.vue
+++ src/components/DatePicker.vue
@@ -232,7 +232,6 @@ html[dir="rtl"] {
   flex-direction: row;
   align-items: center;
   justify-content: center;
-  margin: 10px 0;
   box-shadow: 0px 1px 3px rgba(0, 0, 0, 0.15);
   border-radius: 16px;
   font-weight: bold;
