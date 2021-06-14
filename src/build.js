77bf1a9ee4d73b31503f08cc449ef23c74e74e60
MehranTaheri <mehrant7510@gmail.com>
2021-06-14 18:23:25 +0430

    Add types

--- src/build.js
+++ src/build.js
@@ -1,2 +1,25 @@
 import CustomizableDatePicker from "./components/CustomizableDatePicker";
+// import DatePicker from "./components/DatePicker.vue";
+
+// const components = {
+//   CustomizableDatePicker,
+//   DatePicker,
+// };
+
+// // eslint-disable-next-line @typescript-eslint/explicit-module-boundary-types
+// function install(Vue) {
+//   if (install.installed) return;
+//   install.installed = true;
+//   Object.entries(components).forEach(([componentName, component]) => {
+//     Vue.component(componentName, component);
+//   });
+// }
+
+// if (typeof window !== "undefined" && window.Vue) {
+//   install(window.Vue);
+// }
+
+// export default install;
+
 export default CustomizableDatePicker;
+export { default as DatePicker } from "./components/DatePicker.vue";
---
+++ src/types.d.ts
@@ -0,0 +1,7 @@
+import { VueConstructor } from "vue/types/umd";
+import "./components/CustomizableDatePicker/types";
+
+export * from "./components/CustomizableDatePicker/types";
+declare const CustomizableDatePicker: VueConstructor;
+export default CustomizableDatePicker;
+export const DatePicker: VueConstructor;
